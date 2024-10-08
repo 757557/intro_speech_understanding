0dd7b3ef6b833ede6a90a14fc276816666af4a36
M23W7364 梁佳玥 <1604318299@qq.com>
2024-10-08 17:34:44 +0900

    homework

--- 2024_fall/lec02/homework2.py
+++ 2024_fall/lec02/homework2.py
@@ -5,8 +5,8 @@ c =  55 * 234
 name1 =  "KCGI"
 name2 ="Kyoto"
 d = name1 + ' @ ' + name2
-e =  round(3.458, 2)
-f =  str(57)
+e = round(3.458, 2)
+f = str(57)
 g = 5==9
 h =str(-23.0)
 i =float(False)
